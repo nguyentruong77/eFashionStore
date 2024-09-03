e0fbaa26e80516ea94a34ea986aebea7e894b258
nguyentruong <nguyenichtruong77@gmail.com>
2024-09-03 09:39:56 +0700

    add templete admin
    add templete admin

---
+++ eFashionStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using eFashionStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace eFashionStore.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        eFashionStoreDataContext da = new eFashionStoreDataContext();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            List<SanPham> ds = da.SanPhams.OrderByDescending(p => p.MaSP).ToList();
+            return View(ds);
+        }
+    }
+}
