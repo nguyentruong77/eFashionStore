bc337b5fc72e0f349d1573ddf06e357c74e89b16
Lê Minh Tính <133968641+startinhs@users.noreply.github.com>
2024-09-03 12:53:11 +0700

    tasked: Account (login, register, forgotpass, resetpass, mailservice,

--- eFashionStore/Global.asax.cs
+++ eFashionStore/Global.asax.cs
@@ -1,5 +1,7 @@
+using eFashionStore.MailService;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +15,54 @@ namespace eFashionStore
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //mail
+            var mailSettings = new MailSettings
+            {
+                Mail = ConfigurationManager.AppSettings["MailSettings:Mail"],
+                DisplayName = ConfigurationManager.AppSettings["MailSettings:DisplayName"],
+                Password = ConfigurationManager.AppSettings["MailSettings:Password"],
+                Host = ConfigurationManager.AppSettings["MailSettings:Host"],
+                Port = int.TryParse(ConfigurationManager.AppSettings["MailSettings:Port"], out int port) ? port : 0,
+                EnableSsl = bool.TryParse(ConfigurationManager.AppSettings["MailSettings:EnableSsl"], out bool enableSsl) ? enableSsl : false
+            };
+
+            // check value
+            if (string.IsNullOrEmpty(mailSettings.Mail) ||
+                string.IsNullOrEmpty(mailSettings.DisplayName) ||
+                string.IsNullOrEmpty(mailSettings.Password) ||
+                string.IsNullOrEmpty(mailSettings.Host) ||
+                mailSettings.Port <= 0)
+            {
+                throw new ArgumentException("Mail settings cannot be null or empty.");
+            }
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(mailSettings));
+        }
+
+        public class UnityDependencyResolver : IDependencyResolver
+        {
+            private readonly MailSettings _mailSettings;
+
+            public UnityDependencyResolver(MailSettings mailSettings)
+            {
+                _mailSettings = mailSettings;
+            }
+
+            public object GetService(Type serviceType)
+            {
+                if (serviceType == typeof(SendMailService))
+                {
+                    return new SendMailService(_mailSettings);
+                }
+
+                return null;
+            }
+
+            public IEnumerable<object> GetServices(Type serviceType)
+            {
+                return new List<object>();
+            }
         }
     }
 }
---
+++ eFashionStore/MailService/SendMailService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace eFashionStore.MailService
+{
+    public class SendMailService
+    {
+        private readonly MailSettings _mailSettings;
+
+        public SendMailService(MailSettings mailSettings)
+        {
+            _mailSettings = mailSettings;
+        }
+
+        public async Task<int> SendMail(MailContent mailContent)
+        {
+            var smtpClient = new SmtpClient(_mailSettings.Host)
+            {
+                Port = _mailSettings.Port,
+                Credentials = new NetworkCredential(_mailSettings.Mail, _mailSettings.Password),
+                EnableSsl = _mailSettings.EnableSsl
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_mailSettings.Mail, _mailSettings.DisplayName),
+                Subject = mailContent.Subject,
+                Body = mailContent.Body,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(mailContent.To);
+
+            try
+            {
+                await smtpClient.SendMailAsync(mailMessage);
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+    }
+
+    public class MailContent
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
