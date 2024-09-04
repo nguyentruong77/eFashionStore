62d663cae4c5fb1b4ea38dedb2a89fb2a9376661
nguyentruong <nguyenichtruong77@gmail.com>
2024-09-04 22:23:04 +0700

    Task: Order
    Detail, list, delete order

--- eFashionStore/Areas/Admin/Controllers/CustomerAdminController.cs
+++ eFashionStore/Areas/Admin/Controllers/CustomerAdminController.cs
@@ -76,5 +76,116 @@ namespace eFashionStore.Areas.Admin.Controllers
                 return RedirectToAction("Error404", "HomeAdmin");
             }
         }
+        public ActionResult ListOrder(int? page)
+        {
+            int pageSize = 10;
+            List<HoaDon> od = da.HoaDons.ToList();
+            int pageNumber = (page ?? 1);
+            IPagedList<HoaDon> pagedOD = od.ToPagedList(pageNumber, pageSize);
+            return View(pagedOD);
+        }
+        public ActionResult DetailOrder(string id)
+        {
+            List<ChiTietHoaDon> cthd = da.ChiTietHoaDons.Where(x => x.MaHD.Equals(id)).ToList();
+            return View(cthd);
+        }
+        public ActionResult UpdateOrder(string id)
+        {
+            try
+            {
+                HoaDon hd = da.HoaDons.FirstOrDefault(x => x.MaHD.Equals(id));
+                return View(hd);
+            }
+            catch
+            {
+                return RedirectToAction("Error404", "HomeAdmin");
+            }
+        }
+        //[HttpPost]
+        //public ActionResult UpdateCus(NguoiDung nd)
+        //{
+        //    try
+        //    {
+        //        List<NguoiDung> lnd = da.NguoiDungs.ToList();
+        //        var ndg = da.NguoiDungs.FirstOrDefault(kh => kh.UserID == nd.UserID);
+        //        lnd.Remove(ndg);
+        //        var checkEmail = lnd.FirstOrDefault(x => x.Email == nd.Email);
+        //        var checkSDT = lnd.FirstOrDefault(x => x.SDT == nd.SDT);
+        //        if (checkEmail != null && checkSDT == null)
+        //        {
+        //            ViewBag.EmailError = "Email đã tồn tại!";
+        //            return View();
+        //        }
+        //        else if (checkEmail == null && checkSDT != null)
+        //        {
+        //            ViewBag.SDTError = "Số điện thoại đã tồn tại!";
+        //            return View();
+        //        }
+        //        else if (checkEmail != null && checkSDT != null)
+        //        {
+        //            ViewBag.EmailError = "Email đã tồn tại!";
+        //            ViewBag.SDTError = "Số điện thoại đã tồn tại!";
+        //            return View();
+        //        }
+        //        else
+        //        {
+        //            ndg.HoTen = nd.HoTen;
+        //            ndg.Email = nd.Email;
+        //            ndg.DiaChi = nd.DiaChi;
+        //            ndg.SDT = nd.SDT;
+        //            da.SubmitChanges();
+        //            return RedirectToAction("ListCus", "CustomerAdmin");
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        return RedirectToAction("Error404", "HomeAdmin");
+        //    }
+        //}
+        public ActionResult DelOrder(String id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Error404", "HomeAdmin");
+            }
+            HoaDon hd  = da.HoaDons.FirstOrDefault(s => s.MaHD.Equals(id));
+            
+            if (hd == null)
+            {
+                return RedirectToAction("Error404", "HomeAdmin");
+            }
+
+            return View(hd);
+        }
+        [HttpPost]
+        public ActionResult DelOrder(String id, FormCollection collection)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Error404", "HomeAdmin");
+            }
+            HoaDon hd = da.HoaDons.FirstOrDefault(s => s.MaHD.Equals(id));
+            if (hd == null)
+            {
+                return RedirectToAction("Error404", "HomeAdmin");
+            }    
+            try
+            {
+                List<ChiTietHoaDon> cthd = da.ChiTietHoaDons.Where(s => s.MaHD.Equals(hd.MaHD)).ToList();
+                foreach (ChiTietHoaDon i in cthd)
+                {
+                    da.ChiTietHoaDons.DeleteOnSubmit(i);
+                    da.SubmitChanges();
+                }
+                da.HoaDons.DeleteOnSubmit(hd);
+                da.SubmitChanges();
+                return RedirectToAction("ListOrder", "CustomerAdmin");
+            }
+            catch
+            {
+                return View(hd);
+            }
+        }
+
     }
 }
